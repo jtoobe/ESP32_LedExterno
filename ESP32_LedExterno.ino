1330afd7b6baff1d9e626b803987d4f428858c45
jtoobe <125473983+jtoobe@users.noreply.github.com>
2023-09-04 17:14:41 -0300

    Update ESP32_LedExterno.ino

--- ESP32_LedExterno.ino
+++ ESP32_LedExterno.ino
@@ -8,18 +8,14 @@ const int LedPin=32;
 
 // la funcion Setup se ejecuta cuando se presiona el boton de RESET o se da energia a la placa
 void setup() {
-  // Inicializo el pin digital LED_BUILTIN como output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  
   // Inicializo el pin digital LedPin como output.
   pinMode(LedPin,OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // enciende el LED  (HIGH es el nivel de salida)
-  delay(1000);                       // espera un segundo (1000 milisegundos)
-  digitalWrite(LED_BUILTIN, LOW);    // apago LED poniendo la salida en LOW
-  delay(1000); 
+ 
   digitalWrite(LedPin, HIGH);       // enciende el LED  (HIGH es el nivel de salida)
   delay(1000);                      // espera un segundo (1000 milisegundos)
   digitalWrite(LedPin, LOW);        // apago LED poniendo la salida en LOW
